912a0ff4b369859ea768beac47f489f313ab8f03
dielawn <dielawnmerkill@gmail.com>
2025-03-29 07:08:48 -0600

    updated readme

--- src/analysisTools.js
+++ src/analysisTools.js
@@ -1,6 +1,30 @@
 // calc savings rate
-function calcPreTaxSavingsRate(payStubSummary, ira, savings) {
+function calcSavingsRate(payStubSummary, ira, savings) {
     const totalPayPeriodSavings = payStubSummary.preTaxSavings + ira + savings
     const savingsRate = totalPayPeriodSavings / payStubSummary.totalNetPay * 100
+    return savingsRate
+} 
 
+//401k percentage
+function calc401kPercent(contributionDollar, grossPay) {
+   const contributionPercent = contributionDollar / grossPay
+   return contributionPercent
+}
+
+//calc projections
+function projections(payInterval, acct) {
+    switch(payInterval) {
+        case 'weekly':
+          const monthly = acct.contributionDollar * 4
+          const annually = acct.contributionDollar * 52
+          break;
+        case 'bi-weekly':
+          // code block
+          break;
+        case 'monthly':
+          // code block
+          break;
+        default:
+          // code block
+      }
 } 
